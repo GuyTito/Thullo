7357591cae4e522a5430c51c6e265aadd3635ac3
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-23 17:30:31 +0000

    route to create new board

---
+++ backend/routes/boardsRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const boardsController = require('../controllers/boardsController')
+// const verifyJWT = require("../middleware/verifyJWT");
+
+
+// router.use(verifyJWT)
+
+router.route('/')
+  // .get(boardsController.getAllBoards)
+  .post(boardsController.createNewBoard)
+  // .patch(boardsController.updateBoard)
+  // .delete(boardsController.deleteBoard)
+
+module.exports = router
