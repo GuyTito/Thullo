c15c79c8e2ffb7114b8d8e4ce36727bb4ea3a12c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-25 21:03:47 +0000

    add logo in header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,49 @@
+import styled from 'styled-components';
 
 export default function App() {
-  
-  
+
+
   return (
     <>
-      Thullo, here we go!
+      <Header>
+        <div className='logo'>
+          <div className='design'>
+            <div></div>
+            <div></div>
+          </div>
+          <span>Thullo</span>
+        </div>
+      </Header>
     </>
   )
 }
+
+const Header = styled.header`
+  .logo{
+    display: flex;
+    gap: 10px;
+    align-items: center;
+
+    .design{
+      display: flex;
+      gap: 5px;
+      height: 30px;
+
+      div{
+        width: 14px;
+        background-color: var(--mainColor);
+        border-radius: 3px;
+
+        &:nth-child(2){
+          height: 17px;
+        }
+      }
+    }
+      
+    span{
+      font-size: 18px;
+      font-weight: 600;
+      line-height: 27px;
+    }
+  }
+`
