741d488ee94ae4a176a5aee068fcf05d4c782a46
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-26 12:12:48 +0000

    import and use Topbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,13 @@
-import styled from 'styled-components';
+import Topbar from "./components/Topbar";
 
 export default function App() {
 
 
   return (
     <>
-      <Header>
-        <div className='logo'>
-          <div className='design'>
-            <div></div>
-            <div></div>
-          </div>
-          <span>Thullo</span>
-        </div>
-      </Header>
+      <Topbar />
     </>
   )
 }
 
-const Header = styled.header`
-  .logo{
-    display: flex;
-    gap: 10px;
-    align-items: center;
 
-    .design{
-      display: flex;
-      gap: 5px;
-      height: 30px;
-
-      div{
-        width: 14px;
-        background-color: var(--mainColor);
-        border-radius: 3px;
-
-        &:nth-child(2){
-          height: 17px;
-        }
-      }
-    }
-      
-    span{
-      font-size: 18px;
-      font-weight: 600;
-      line-height: 27px;
-    }
-  }
-`
