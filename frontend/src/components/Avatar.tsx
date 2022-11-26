8d077c8cf1bd74e803d178fb89f3086f73d58dea
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-26 17:54:41 +0000

    create Avatar component

---
+++ frontend/src/components/Avatar.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export default function Avatar() {
+  
+  
+  return (
+    <>
+      <Div>
+        {/* <img src="" alt="" /> */}
+      </Div>
+    </>
+  )
+}
+
+const Div = styled.div`
+  height: 34px;
+  width: 34px;
+  border-radius: 8px;
+  overflow: hidden;
+  background-color: var(--gray);
+`
