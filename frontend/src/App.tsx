2910f67181511604550e7262000fd60a658d3d36
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-26 18:00:13 +0000

    use Home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Topbar from "./components/Topbar";
+import Home from "./pages/Home";
 
 export default function App() {
 
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <>
       <Topbar />
+      <Home />
     </>
   )
 }
