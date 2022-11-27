d2f8753778246e8d7e59ba0cd438029b267eabc5
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-27 17:20:57 +0000

    refresh endpoint to get new access token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -125,7 +125,36 @@ const login = asyncHandler(async (req, res) => {
 // @route POSt /auth/refresh
 // @access Public
 const refresh = asyncHandler(async (req, res) => {
+  const cookies = req.cookies
 
+  if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' })
+
+  const refreshToken = cookies.jwt
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) return res.status(403).json({ message: 'Forbidden' })
+
+      const foundUser = await User.findOne({ email: decoded.email }).exec()
+
+      if (!foundUser) return res.status(401).json({ message: 'Unauthorized' })
+
+      const accessToken = jwt.sign(
+        {
+          "UserInfo": {
+            "fullname": foundUser.fullname,
+            "email": foundUser.email
+          }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '15m' }
+      )
+
+      res.json({ accessToken })
+    })
+  )
 })
 
 
