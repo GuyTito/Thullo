6aacb9c30a777f7aac709985988974a07732de2b
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-29 18:21:30 +0000

    Create authSlice.ts

---
+++ frontend/src/store/authSlice.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+const initialState = {
+  auth: {}
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+})
+
+export default authSlice.reducer;
