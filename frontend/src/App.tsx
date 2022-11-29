a26ace443394b27f53efcf3a46d4deec01f9a7dd
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-29 19:24:33 +0000

    protect some routes with RequireAuth component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import RequireAuth from "./components/RequireAuth";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
@@ -12,8 +13,10 @@ export default function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+          </Route>
         </Route>
       </Routes>
       </BrowserRouter>
