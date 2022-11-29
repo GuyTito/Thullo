43c603fa6a185fc2c81762406bd53374781a2050
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-29 18:19:01 +0000

    setup client-side routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
-import Topbar from "./components/Topbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 
 export default function App() {
 
 
   return (
     <>
-      <Topbar />
-      <Home />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
