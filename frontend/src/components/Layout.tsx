5575fb0944e0d42806a4f240434fc5ee0f972d26
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-29 18:17:52 +0000

    Create Layout.tsx

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import Topbar from "./Topbar";
+import { Outlet } from 'react-router-dom';
+
+
+export default function Layout() {
+  
+  
+  return (
+    <>
+      <Topbar />
+
+      <Outlet />
+    </>
+  )
+}
