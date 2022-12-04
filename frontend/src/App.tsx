a78f82e4f301d22dd4769847c107c9fb70443dab
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-04 19:38:38 +0000

    add error page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import PersistLogin from "./components/PersistLogin";
 import RequireAuth from "./components/RequireAuth";
 import Boards from "./pages/Boards";
+import ErrorPage from "./pages/ErrorPage";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -14,21 +15,24 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<LandingPage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<LandingPage />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
 
-          <Route element={<PersistLogin />}>
-            <Route element={<RequireAuth />}>
-              <Route path="boards">
-                <Route index element={<Boards />} />
+            <Route element={<PersistLogin />}>
+              <Route element={<RequireAuth />}>
+                <Route path="boards">
+                  <Route index element={<Boards />} />
+                </Route>
               </Route>
             </Route>
+
+            {/* error path */}
+            <Route path='*' element={<ErrorPage />} />
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
       </BrowserRouter>
     </>
   )
