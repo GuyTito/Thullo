4277904619fa87bbf7fcdf46f0af65391a3edd0b
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-04 15:55:11 +0000

    check auth before showing topbar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,13 +1,16 @@
 import Topbar from "./Topbar";
 import { Outlet } from 'react-router-dom';
+import { useAppSelector } from "../store/hooks";
+import { getAuth } from "../store/authSlice";
 
 
 export default function Layout() {
-  
+  const auth = useAppSelector(getAuth);
+
   
   return (
     <>
-      <Topbar />
+      {auth.accessToken && <Topbar />}
 
       <Outlet />
     </>
