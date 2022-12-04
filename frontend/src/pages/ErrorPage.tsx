b3d2a5959b3a632cc1f0995ccecc563b23b89b18
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-04 19:38:14 +0000

    Create ErrorPage.tsx

---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export default function ErrorPage() {
+  
+  
+  return (
+    <Div>
+      404 Not found. ☹
+    </Div>
+  )
+}
+
+
+const Div = styled.div`
+  
+`
