af0bd82949308098c4382ef00f54fa64eab3ebbe
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-05 15:25:48 +0000

    remove auth of topbar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,13 @@
 import Topbar from "./Topbar";
 import { Outlet } from 'react-router-dom';
-import { useAppSelector } from "../store/hooks";
-import { getAuth } from "../store/authSlice";
 
 
 export default function Layout() {
-  const auth = useAppSelector(getAuth);
 
   
   return (
     <>
-      {auth.accessToken && <Topbar />}
+      <Topbar />
 
       <Outlet />
     </>
