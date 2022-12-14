7a22578f25e882bd0e42bb410e2b24ee0d341b40
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-14 18:55:32 +0000

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,7 +4,8 @@ const BASE_URL = 'http://localhost:3000';
 
 
 export default axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
 });
 
 export const axiosPrivate = axios.create({
