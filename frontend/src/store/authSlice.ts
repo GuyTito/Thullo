eb01298fdc34f8f2f732ace11a6a6ff22d7465b4
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-15 16:27:29 +0000

    handle error for getNewAccessToken

--- frontend/src/store/authSlice.ts
+++ frontend/src/store/authSlice.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AxiosError } from "axios";
 import axios from "../api/axios";
 import { RootState } from "./store";
 
@@ -6,19 +7,24 @@ import { RootState } from "./store";
 interface authState {
   accessToken: string
   persist: boolean
+  errorMsg?: unknown
 }
 
 const initialState: authState = {
   accessToken: '',
-  persist: JSON.parse(localStorage.getItem('persist') as string) || false
+  persist: JSON.parse(localStorage.getItem('persist') as string) || false,
 }
 
-export const getNewAccessToken = createAsyncThunk('auth/getNewAccessToken', async ()=>{
-  const response = await axios.get('/auth/refresh', {
-    withCredentials: true
-  });
-  const accessToken = response?.data?.accessToken;
-  return accessToken
+export const getNewAccessToken = createAsyncThunk('auth/getNewAccessToken', async (_, thunkAPI)=>{
+  try {
+    const response = await axios.get('/auth/refresh', {
+      withCredentials: true,
+    })
+    const accessToken = response?.data?.accessToken;
+    return accessToken
+  } catch (error: AxiosError | any) {
+    return thunkAPI.rejectWithValue(`${error.message}. ${error.response.data.message}`)
+  }
 })
 
 export const logout = createAsyncThunk('auth/logout', async(_, thunkAPI)=>{
@@ -43,6 +49,10 @@ const authSlice = createSlice({
     builder
       .addCase(getNewAccessToken.fulfilled, (state, action: PayloadAction<string>) => {
         state.accessToken = action.payload
+      })
+      .addCase(getNewAccessToken.rejected, (state, action) => {
+        state.errorMsg = action.payload
+        console.log('refresh error', state.errorMsg)
       })
   }
 })
