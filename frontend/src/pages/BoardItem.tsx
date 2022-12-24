14cde8add857dbcdab0d78e1f395cfb288befd12
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-24 19:40:30 +0000

    Create BoardItem.tsx

---
+++ frontend/src/pages/BoardItem.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export default function BoardItem() {
+  
+  
+  return (
+    <Div>
+      board item
+    </Div>
+  )
+}
+
+
+const Div = styled.div`
+  
+`
