5c53d0b2b619671da9d7c877502f3561df0ea05e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-29 11:56:43 +0000

    add add-list button

--- frontend/src/pages/BoardItem.tsx
+++ frontend/src/pages/BoardItem.tsx
@@ -19,6 +19,11 @@ export default function BoardItem() {
         </div>
         <button className="btn btn-gray">... Show Menu</button>
       </div>
+      <div className="lists">
+        <div className="add-list">
+          <button><span>Add list</span> <span>+</span></button>
+        </div>
+      </div>
     </Div>
   )
 }
@@ -40,6 +45,26 @@ const Div = styled.div`
         display: flex;
         gap: 12px;
         align-items: center;
+      }
+    }
+  }
+  .lists{
+    background-color: var(--bgColor);
+    margin-top: 24px;
+    border-radius: 24px;
+    padding: 24px;
+    .add-list{
+      width: 240px;
+      button{
+        padding: 8px 13px;
+        color: var(--mainColor);
+        background: var(--lightBlue);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-radius: 8px;
+        font-size: 12px;
+        width: 100%;
       }
     }
   }
