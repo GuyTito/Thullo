ef1d86bc083ff7c610c1310a33be009e9a335246
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-12-29 10:47:19 +0000

    add topbar

--- frontend/src/pages/BoardItem.tsx
+++ frontend/src/pages/BoardItem.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Avatar from "../components/Avatar";
 
 
 export default function BoardItem() {
@@ -6,12 +7,40 @@ export default function BoardItem() {
   
   return (
     <Div>
-      board item
+      <div className="topbar">
+        <div className="left">
+          <button className="btn btn-gray">Private</button>
+          <div className="avatars">
+            {[1, 2, 3].map(i => (
+              <Avatar key={i} />
+            ))}
+            <span>+5 others</span>
+          </div>
+        </div>
+        <button className="btn btn-gray">... Show Menu</button>
+      </div>
     </Div>
   )
 }
 
 
 const Div = styled.div`
-  
+  background-color: var(--white);
+  padding: 24px;
+  .topbar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 12px;
+      .avatars{
+        display: flex;
+        gap: 12px;
+        align-items: center;
+      }
+    }
+  }
 `
