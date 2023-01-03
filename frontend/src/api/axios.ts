a288e119cd30e23c94d993139e35405a0f4212ee
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-03 21:22:50 +0000

    add withCredentials
     to  add cookies when requesting

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -5,7 +5,8 @@ const BASE_URL = 'http://localhost:3000';
 
 export default axios.create({
   baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" }
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
