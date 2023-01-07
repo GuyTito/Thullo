3843308cd4585b6b0b3f219a8b3726a91a38cfa5
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-07 13:24:05 +0000

    Create InviteUser.tsx

---
+++ frontend/src/components/InviteUser.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export default function InviteUser() {
+  
+  
+  return (
+    <Div>
+      invite user
+    </Div>
+  )
+}
+
+
+const Div = styled.div`
+  
+`
