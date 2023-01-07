e69c5385a47b2c0fc74d518499aaf2ec04a1baf5
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-07 13:00:53 +0000

    extract content to boardMenu component

--- frontend/src/pages/BoardItem.tsx
+++ frontend/src/pages/BoardItem.tsx
@@ -1,5 +1,5 @@
-import { FaLock, FaUserCircle } from "react-icons/fa";
-import { MdComment, MdEdit, MdGroups } from "react-icons/md";
+import { FaLock, } from "react-icons/fa";
+import { MdComment, } from "react-icons/md";
 import { TbDots } from "react-icons/tb";
 import { TfiClip } from "react-icons/tfi";
 import { useNavigate, useParams } from "react-router-dom";
@@ -13,11 +13,8 @@ import { getCurrentBoard, setCurrentBoard } from "../store/boardSlice";
 import Dropdown from "../components/Dropdown";
 import ClickAwayListener from 'react-click-away-listener';
 import { BiWorld } from "react-icons/bi";
-import { IoDocumentText } from "react-icons/io5";
-import { GrClose } from "react-icons/gr";
-import formatDate from "../hooks/formatDate";
 import VisibilityMenu from "../components/VisibilityMenu";
-import useUpdateBoard from "../hooks/useUpdateBoard";
+import BoardMenu from "../components/BoardMenu";
 
 
 export default function BoardItem() {
@@ -28,8 +25,6 @@ export default function BoardItem() {
   const currentBoard = useAppSelector(getCurrentBoard)
   const [open, setOpen] = useState(false);
   const [showBoardMenu, setShowBoardMenu] = useState(false);
-  const [editDesc, setEditDesc] = useState(false);
-  const [description, setDescription] = useState('');
   const ref = useRef(null)
   const boardMenuRef = useRef(null)
 
@@ -54,14 +49,6 @@ export default function BoardItem() {
         console.log(error.response.data.message)
       }
     }
-  }
-
-
-  async function handleDescription(){
-    const boardUpdate = { description }
-    // if (id) useUpdateBoard(id, boardUpdate)
-
-    setEditDesc(false)
   }
 
   
@@ -103,61 +90,7 @@ export default function BoardItem() {
               button = {
                 <button onClick={() => setShowBoardMenu(!showBoardMenu)} className="btn-pad btn-gray"><TbDots /> Show Menu</button>
               }
-              content = {
-                <div className="board-menu">
-                  <div className="header">
-                    <h3>Menu</h3>
-                    <button onClick={() => setShowBoardMenu(false)}><GrClose /></button>
-                  </div>
-                  <hr />
-                  <span className="made-by"><FaUserCircle /> Made by</span>
-                  <div className="creator">
-                    <Avatar /> 
-                    <div>
-                      <span>Daniel Akrofi</span>
-                      {currentBoard?.createdAt && <span>on {formatDate(currentBoard?.createdAt)}</span>}
-                    </div>
-                  </div>
-                  <div className="desc">
-                    <span><IoDocumentText /> Description</span>
-                    {!editDesc && <button onClick={()=>setEditDesc(true)}><MdEdit /> Edit</button>}
-                  </div>
-                  {editDesc ? 
-                    <>
-                      <textarea onChange={(e) => setDescription(e.target.value)} className="description" defaultValue={currentBoard?.description} >
-                      </textarea> 
-                      <p className="resize">* You can resize the height of the text box on its bottom right corner.</p>
-                      <div className="submit">
-                        <button onClick={() => handleDescription()} className="btn-pad btn-main">Save</button>
-                        <button onClick={() => setEditDesc(false)}>Cancel</button>
-                      </div>
-                    </>
-                    :
-                    <div className="description">
-                      {currentBoard?.description}
-                    </div>
-                  }
-                  <div className="team"><MdGroups /> Team</div>
-                  <div className="members">
-                    <div>
-                      <div className="name-avatar">
-                        <Avatar />
-                        <span>Daniel Akrofi</span>
-                      </div>
-                      <span className="admin">Admin</span>
-                    </div>
-                    {[1, 2].map(i => (
-                      <div>
-                        <div className="name-avatar">
-                          <Avatar />
-                          <span>Bianca Soulsa</span>
-                        </div>
-                        <button className="btn-error">Remove</button>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              }
+              content={<BoardMenu setShowBoardMenu={setShowBoardMenu} />}
             />
           </ClickAwayListener>
         </div>
@@ -237,125 +170,6 @@ const Div = styled.div`
         margin-top: 0;
         padding-left: 20px;
         padding-right: 20px;
-      }
-      .board-menu{
-        .header{
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          h3{
-            font-weight: 600;
-          }
-        }
-        hr{
-          margin: 10px 0;
-          color: var(--gray);
-        }
-        .made-by{
-          display: flex;
-          align-items: center;
-          gap: 5px;
-          color: var(--gray);
-          font-size: 12px;
-          margin-bottom: 13px;
-        }
-        .creator{
-          display: flex;
-          align-items: center;
-          gap: 12px;
-          div{
-            display: flex;
-            flex-direction: column;
-            margin-bottom: 20px;
-            span:first-of-type{
-              font-weight: 600;
-            }
-            span:last-of-type{
-              color: var(--gray);
-              font-size: 12px;
-            }
-          }
-        }
-        .desc{
-          display: flex;
-          align-items: center;
-          gap: 20px;
-          font-size: 12px;
-          color: var(--gray);
-          margin-bottom: 10px;
-          span{
-            display: flex;
-            align-items: center;
-            gap: 5px;
-          }
-          button{
-            display: flex;
-            align-items: center;
-            gap: 5px;
-            border: 1px solid var(--gray);
-            border-radius: 8px;
-            padding: 2px 8px;
-          }
-        }
-        .description{
-          width: 300px;
-          font-size: 14px;
-          max-height: 400px;
-          overflow-y: overlay;
-          padding-right: 10px;
-        }
-        textarea{
-          border: 1px solid var(--gray);
-          height: 200px;
-          padding: 10px;
-          border-radius: 8px;
-          outline-color: var(--mainColor);
-        }
-        .resize{
-          font-size: 10px;
-          color: var(--gray);
-          width: 300px;
-        }
-        .submit{
-          display: flex;
-          gap: 12px;
-          font-size: 12px;
-        }
-        .team{
-          display: flex;
-          align-items: center;
-          gap: 5px;
-          font-size: 12px;
-          color: var(--gray);
-          margin-top: 24px;
-          margin-bottom: 10px;
-        }
-        .members{
-          div{
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            .name-avatar{
-              gap: 12px;       span{
-                font-weight: 600;
-              }
-            }
-            .admin{
-              color: var(--gray);
-              font-size: 12px;
-            }
-            button{
-              font-size: 12px;
-              color: var(--error);
-              padding: 2px 8px;
-              border-radius: 8px;
-              border: 1px solid var(--error);
-            }
-          }
-          div:not(:last-of-type){
-            margin-bottom: 10px;
-          }
-        }
       }
     }
   }
