b8e4bfc80fb00e461c83a86f640100eaebd46789
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-12 12:19:12 +0000

    add handleSubmit function

--- frontend/src/components/InviteUser.tsx
+++ frontend/src/components/InviteUser.tsx
@@ -1,16 +1,23 @@
+import { useState, FormEvent } from "react";
 import { BiSearch } from "react-icons/bi";
 import styled from "styled-components";
 
 
 export default function InviteUser() {
-  
+  const [searchTerm, setSearchTerm] = useState('')
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>){
+    e.preventDefault()
+    console.log('search Term', searchTerm)
+    setSearchTerm('')
+  }
   
   return (
     <Div>
       <h4>Invite user</h4>
       <p>Search users you want to invite to</p>
-      <form className="search">
-        <input type="text" placeholder='User...' />
+      <form onSubmit={(e)=>handleSubmit(e)} className="search">
+        <input onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} type="text" placeholder='User...' />
         <button className='btn-main btn-square'><BiSearch /></button>
       </form>
     </Div>
@@ -23,24 +30,18 @@ const Div = styled.div`
     font-weight: 600;
   }
   p{
-    /* font-size: 12px; */
+    font-size: 13px;
     color: var(--gray);
   }
   .search{
     margin-top: 13px;
-    padding-left: 12px;
     display: flex;
     border-radius: 8px;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
     padding: 4px;
 
     input{
-      padding-left: 13px;
       outline: none;
-
-      /* &::placeholder{
-        color: var(--gray);
-      } */
     }
   }
 `
