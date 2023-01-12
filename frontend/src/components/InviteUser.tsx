53d10f8ad9a874346fc66aa7787ae63c7bedb141
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-12 09:49:40 +0000

    structure and styling

--- frontend/src/components/InviteUser.tsx
+++ frontend/src/components/InviteUser.tsx
@@ -1,3 +1,4 @@
+import { BiSearch } from "react-icons/bi";
 import styled from "styled-components";
 
 
@@ -6,12 +7,40 @@ export default function InviteUser() {
   
   return (
     <Div>
-      invite user
+      <h4>Invite user</h4>
+      <p>Search users you want to invite to</p>
+      <form className="search">
+        <input type="text" placeholder='User...' />
+        <button className='btn-main btn-square'><BiSearch /></button>
+      </form>
     </Div>
   )
 }
 
 
 const Div = styled.div`
-  
+  h4{
+    font-weight: 600;
+  }
+  p{
+    /* font-size: 12px; */
+    color: var(--gray);
+  }
+  .search{
+    margin-top: 13px;
+    padding-left: 12px;
+    display: flex;
+    border-radius: 8px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+    padding: 4px;
+
+    input{
+      padding-left: 13px;
+      outline: none;
+
+      /* &::placeholder{
+        color: var(--gray);
+      } */
+    }
+  }
 `
