ff3d1c4efa2207cbf29788e691f7235d3faa61bf
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-13 18:51:20 +0000

    Create listSlice.ts

---
+++ frontend/src/store/listSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "./store"
+
+
+interface ListType {
+  boardId: string
+  title: string
+  createdAt: string
+  updatedAt: string
+}
+
+interface listState {
+  currentLists: ListType[]
+}
+
+const initialState: listState = {
+  currentLists: []
+}
+
+const listSlice = createSlice({
+  name: "list",
+  initialState,
+  reducers: {
+    addNewList: (state, action: PayloadAction<ListType>) => {
+      state.currentLists.push(action.payload)
+    },
+    loadLists: (state, action: PayloadAction<ListType[]>) => {
+      state.currentLists = action.payload
+    },
+  },
+})
+
+export const getCurrentLists = (state: RootState) => state.list
+
+export const { addNewList, loadLists } = listSlice.actions
+
+export default listSlice.reducer;
