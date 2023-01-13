eed580b90c962c1d9f47155b1a9049e5817e0a9c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-13 18:51:35 +0000

    add list reducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import boardReducer from "./boardSlice";
+import listReducer from "./listSlice";
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     board: boardReducer,
+    list: listReducer,
   }
 })
 
