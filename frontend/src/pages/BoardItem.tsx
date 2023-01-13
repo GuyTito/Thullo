aa44b6d4ddc691dbc9e2bc244ee1ca0ea81b1f56
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-13 13:25:02 +0000

    add modal to add new list form

--- frontend/src/pages/BoardItem.tsx
+++ frontend/src/pages/BoardItem.tsx
@@ -5,7 +5,7 @@ import { TfiClip } from "react-icons/tfi";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Avatar from "../components/Avatar";
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, FormEvent } from 'react';
 import interceptedAxiosPrivate from "../hooks/interceptedAxiosPrivate";
 import { AxiosError } from "axios";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
@@ -16,6 +16,7 @@ import { BiWorld } from "react-icons/bi";
 import VisibilityMenu from "../components/VisibilityMenu";
 import BoardMenu from "../components/BoardMenu";
 import InviteUser from "../components/InviteUser";
+import Modal from "../components/Modal";
 
 
 export default function BoardItem() {
@@ -27,6 +28,8 @@ export default function BoardItem() {
   const [open, setOpen] = useState(false);
   const [showBoardMenu, setShowBoardMenu] = useState(false);
   const [showInviteUser, setShowInviteUser] = useState(false);
+  const [showListModal, setShowListModal] = useState(false);
+  const [listName, setListName] = useState('');
   const ref = useRef(null)
   const boardMenuRef = useRef(null)
   const inviteUserRef = useRef(null)
@@ -54,8 +57,6 @@ export default function BoardItem() {
     }
   }
 
-  
-
   useEffect(()=>{
     if (id) getBoard(id)
 
@@ -64,89 +65,122 @@ export default function BoardItem() {
     }
   }, [])
 
+  async function addList(e: FormEvent<HTMLFormElement>){
+    e.preventDefault()
+    // const response = await axiosPrivate.patch('/boards', { _id, boardUpdate }, {
+    //   headers: { 'Content-Type': 'application/json' }
+    // })
+    console.log('add list', listName)
+    cancelList()
+  }
+
+  function cancelList(){
+    setListName('')
+    setShowListModal(false)
+  }
+
   
   return (
-    <Div>
-      <div className="topbar">
-        <div className="left">
-          <ClickAwayListener onClickAway={()=>setOpen(false)}>
-            <Dropdown open={open} ref={ref}
-              button = {
-                <button onClick={() => setOpen(!open)} className={`btn-pad ${currentBoard?.privacy ? 'btn-selected' : 'btn-gray'}`}>
-                  {currentBoard?.privacy ? <><FaLock /> Private</> : <><BiWorld /> Public</>}
-                </button>
-              }
-              content={<VisibilityMenu setOpen={setOpen} />}
-            />
-          </ClickAwayListener>
-          <div className="avatars">
-            {[1, 2, 3].map(i => (
-              <Avatar key={i} />
-            ))}
-            <ClickAwayListener onClickAway={() => setShowInviteUser(false)}>
-              <Dropdown open={showInviteUser} ref={inviteUserRef} 
+    <>
+      <Div>
+        <div className="topbar">
+          <div className="left">
+            <ClickAwayListener onClickAway={()=>setOpen(false)}>
+              <Dropdown open={open} ref={ref}
                 button = {
-                  <button onClick={() => setShowInviteUser(!showInviteUser)} className="btn-main btn-square">+</button>
+                  <button onClick={() => setOpen(!open)} className={`btn-pad ${currentBoard?.privacy ? 'btn-selected' : 'btn-gray'}`}>
+                    {currentBoard?.privacy ? <><FaLock /> Private</> : <><BiWorld /> Public</>}
+                  </button>
                 }
-                content = {
-                  <InviteUser />
+                content={<VisibilityMenu setOpen={setOpen} />}
+              />
+            </ClickAwayListener>
+            <div className="avatars">
+              {[1, 2, 3].map(i => (
+                <Avatar key={i} />
+              ))}
+              <ClickAwayListener onClickAway={() => setShowInviteUser(false)}>
+                <Dropdown open={showInviteUser} ref={inviteUserRef} 
+                  button = {
+                    <button onClick={() => setShowInviteUser(!showInviteUser)} className="btn-main btn-square">+</button>
+                  }
+                  content = {
+                    <InviteUser />
+                  }
+                />
+              </ClickAwayListener>
+            </div>
+          </div>
+          
+          <div className="right">
+            <ClickAwayListener onClickAway={() => setShowBoardMenu(false)}>
+              <Dropdown open={showBoardMenu} ref={boardMenuRef}
+                button = {
+                  <button onClick={() => setShowBoardMenu(!showBoardMenu)} className="btn-pad btn-gray"><TbDots /> Show Menu</button>
                 }
+                content={<BoardMenu setShowBoardMenu={setShowBoardMenu} />}
               />
             </ClickAwayListener>
           </div>
         </div>
-        
-        <div className="right">
-          <ClickAwayListener onClickAway={() => setShowBoardMenu(false)}>
-            <Dropdown open={showBoardMenu} ref={boardMenuRef}
-              button = {
-                <button onClick={() => setShowBoardMenu(!showBoardMenu)} className="btn-pad btn-gray"><TbDots /> Show Menu</button>
-              }
-              content={<BoardMenu setShowBoardMenu={setShowBoardMenu} />}
-            />
-          </ClickAwayListener>
-        </div>
-      </div>
-      <div className="lists">
-        {[1,2,].map(i => (
-          <div key={i} className="list">
-            <div className="list-title">
-              <span>Backlog 🤔</span>
-              <TbDots />
-            </div>
-            {[1,2].map(i => (
-              <div key={i} className="card">
-                <div className="cover">
-                  {/* <img src="" alt="" /> */}
-                </div>
-                <div className="card-title">✋🏿 Move anything 'ready' here</div>
-                <div className="labels">
-                  <label className="label">Technical</label>
-                </div>
-                <div className="bottom">
-                  <div className="left">
-                    {[1, 2,].map(i => (
-                      <Avatar key={i} />
-                    ))}
-                    <button className="btn-main btn-square">+</button>
-                  </div>
-                  <div className="right">
-                    <span><MdComment />1</span>
-                    <span><TfiClip /> 2</span>
-                  </div>
-                </div>
+        <div className="lists">
+          {[1,2,].map(i => (
+            <div key={i} className="list">
+              <div className="list-title">
+                <span>Backlog 🤔</span>
+                <TbDots />
               </div>
-            ))}
-            <div className="add-another">
-              <button><span>Add card</span> <span>+</span></button>
+              {[1,2].map(i => (
+                <div key={i} className="card">
+                  <div className="cover">
+                    {/* <img src="" alt="" /> */}
+                  </div>
+                  <div className="card-title">✋🏿 Move anything 'ready' here</div>
+                  <div className="labels">
+                    <label className="label">Technical</label>
+                  </div>
+                  <div className="bottom">
+                    <div className="left">
+                      {[1, 2,].map(i => (
+                        <Avatar key={i} />
+                      ))}
+                      <button className="btn-main btn-square">+</button>
+                    </div>
+                    <div className="right">
+                      <span><MdComment />1</span>
+                      <span><TfiClip /> 2</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <div className="add-another">
+                <button><span>Add card</span> <span>+</span></button>
+              </div>
             </div>
+          ))}
+          <div className="add-another">
+            <button onClick={() => setShowListModal(true)}><span>Add list</span> <span>+</span></button>
           </div>
-        ))}
-        <div className="add-another">
-          <button><span>Add list</span> <span>+</span></button>
         </div>
-      </div>
-    </Div>
+      </Div>
+
+      {/* add list modal */}
+      {showListModal &&
+        <Modal>
+          <ClickAwayListener onClickAway={() => setShowListModal(false)}>
+            <ListModal onSubmit={(e) => addList(e)}>
+              <div className="form-control">
+                <input onChange={(e)=>setListName(e.target.value)} value={listName} type="text"  />
+              </div>
+              <div className="bottom">
+                <button type="submit" className="btn-pad btn-main">Add list</button>
+                <button onClick={cancelList} type="button" className="">Cancel</button>
+              </div>
+            </ListModal>
+          </ClickAwayListener>
+        </Modal>
+      }
+    </>
   )
 }
 
@@ -269,5 +303,17 @@ const Div = styled.div`
         width: 100%;
       }
     }
+  }
+`
+
+const ListModal = styled.form`
+  padding: 24px;
+  background-color: var(--white);
+  position: relative;
+  .bottom{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
   }
 `
