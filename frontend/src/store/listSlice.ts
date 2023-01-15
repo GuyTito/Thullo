b98a1b31e96f5666ea3de34959ff67fa4cf7cecb
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-15 13:13:39 +0000

    export ListType

--- frontend/src/store/listSlice.ts
+++ frontend/src/store/listSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { RootState } from "./store"
 
 
-interface ListType {
+export interface ListType {
   _id: string
   boardId: string
   title: string
