b2d03264879c2f1c7606b5d06a91321e795a9950
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-15 12:50:25 +0000

    Create Card.tsx

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,83 @@
+import { MdComment } from "react-icons/md";
+import { TfiClip } from "react-icons/tfi";
+import styled from "styled-components";
+import Avatar from "./Avatar";
+
+
+export default function Card() {
+  
+  
+  return (
+    <Div>
+      <div className="cover">
+        {/* <img src="" alt="" /> */}
+      </div>
+      <div className="card-title">✋🏿 Move anything 'ready' here</div>
+      <div className="labels">
+        <label className="label">Technical</label>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          {[1, 2,].map(i => (
+            <Avatar key={i} />
+          ))}
+          <button className="btn-main btn-square">+</button>
+        </div>
+        <div className="right">
+          <span><MdComment />1</span>
+          <span><TfiClip /> 2</span>
+        </div>
+      </div>
+    </Div>
+  )
+}
+
+
+const Div = styled.div`
+  width: 100%;
+  border-radius: 12px;
+  background-color: var(--white);
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  .cover{
+    height: 130px;
+    width: 100%;
+    background-color: var(--gray);
+    border-radius: 12px;
+  }
+  .labels{
+    display: flex;
+    gap: 12px;
+    label{
+      color: var(--mainColor);
+      background-color: var(--lightBlue);
+      border-radius: 8px;
+      font-size: 10px;
+      padding: 2px 6px;
+    }
+  }
+  .bottom{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left{
+      display: flex;
+      gap: 12px;
+      flex-direction: row;
+      align-items: center;
+    }
+    .right{
+      font-size: 10px;
+      display: flex;
+      gap: 12px;
+      color: var(--gray);
+      span{
+        display: flex;
+        gap: 3px;
+        align-items: center;
+      }
+    }
+  }
+`
