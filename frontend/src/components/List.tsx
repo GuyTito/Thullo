19726ee9d3f1c14618258f91719f7335ea4ac899
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-19 08:43:42 +0000

    add card form in a modal

--- frontend/src/components/List.tsx
+++ frontend/src/components/List.tsx
@@ -1,7 +1,11 @@
+import { useState, useRef } from "react";
+import ClickAwayListener from "react-click-away-listener";
 import { TbDots } from "react-icons/tb";
 import styled from "styled-components";
 import { ListType } from "../store/listSlice";
 import Card from "./Card";
+import Modal from "./Modal";
+import NewCardForm from "./NewCardForm";
 
 
 interface ListProps{
@@ -10,20 +14,36 @@ interface ListProps{
 
 export default function List(props: ListProps) {
   const { list } = props
+  const [showCardFormModal, setShowCardFormModal] = useState(false)
+  const cardFormRef = useRef(null)
+
   
   return (
-    <Div >
-      <div className="list-title">
-        <span>{list.title}</span>
-        <TbDots />
-      </div>
-      {[1].map(i => (
-        <Card key={i} />
-      ))}
-      <div className="add-another">
-        <button><span>Add card</span> <span>+</span></button>
-      </div>
-    </Div>
+    <>
+      <Div >
+        <div className="list-title">
+          <span>{list.title}</span>
+          <TbDots />
+        </div>
+        {[1].map(i => (
+          <Card key={i} />
+        ))}
+        <div className="add-another">
+          <button onClick={() => setShowCardFormModal(true)}>
+            <span>Add card</span> <span>+</span>
+          </button>
+        </div>
+      </Div>
+
+      {/* modal */}
+      {showCardFormModal &&
+        <Modal>
+          <ClickAwayListener onClickAway={() => setShowCardFormModal(false)}>
+            <NewCardForm setShowCardFormModal={setShowCardFormModal} ref={cardFormRef} />
+          </ClickAwayListener>
+        </Modal>
+      }
+    </>
   )
 }
 
