e08e69f7aade5142bf3630a920ea98a9ca2e528f
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-20 18:38:54 +0000

    create card model

---
+++ backend/models/Card.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema(
+  {
+    listId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'List'
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    coverImgUrl: {
+      type: String,
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Card', cardSchema);
