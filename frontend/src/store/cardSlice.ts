e7b69bda7a9710d430bdccc13c2ee79cb0b96bdd
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-01-23 15:05:18 +0000

    Create cardSlice.ts

---
+++ frontend/src/store/cardSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "./store"
+
+
+export interface CardType {
+  _id: string
+  listId: string
+  title: string
+  coverImgUrl: string
+  description: string
+  createdAt: string
+  updatedAt: string
+}
+
+interface cardState {
+  currentCard: CardType | null
+}
+
+const initialState: cardState = {
+  currentCard: null
+}
+
+const cardSlice = createSlice({
+  name: "card",
+  initialState,
+  reducers: {
+    setCurrentCard: (state, action: PayloadAction<CardType | null>) => {
+      state.currentCard = action.payload
+    }
+  },
+})
+
+export const getCurrentCards = (state: RootState) => state.card.currentCard
+
+export const { setCurrentCard, } = cardSlice.actions
+
+export default cardSlice.reducer;
