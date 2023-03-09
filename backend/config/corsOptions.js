d545a1322b5605c3ae7c4b1ea84bd6c9708a1b4d
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-03-09 18:14:10 +0000

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -4,7 +4,8 @@ const allowedOrigins = require("./allowedOrigins")
 const corsOptions = {
   origin: (origin, callback) => {
     // remove !origin when ready to push. !origin for origins whiout headers like postman etc
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
