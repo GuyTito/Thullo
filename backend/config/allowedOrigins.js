b525653583044dacf1592587f666ef638ec51a08
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-03-09 18:14:03 +0000

    allow frontend url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,10 +1,8 @@
 // clear the dev url when ready to push
 
 const allowedOrigins = [
-  'https://yourfrontend.com', 
-  'https://tito-thullo.com', 
-  'https://www.tito-thullo.com', 
-  'http://127.0.0.1:5173', 
+  'https://tito-thullo.onrender.com', 
+  // 'http://127.0.0.1:5173', 
 ]
 
 
