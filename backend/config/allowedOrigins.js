8eb8bbd854e3b89c610d73deede3ccf52bc31292
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-03-10 14:38:03 +0000

    use prod url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 // clear the dev url when ready to push
 
 const allowedOrigins = [
-  // 'https://tito-thullo.onrender.com', 
-  'http://127.0.0.1:5173', 
+  'https://tito-thullo.onrender.com', 
+  // 'http://127.0.0.1:5173', 
 ]
 
 
